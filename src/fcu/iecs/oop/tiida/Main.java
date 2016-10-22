8df177b636b886c640227f6471ce9ed1d50bb22d
承翰 <@MSI>
2016-10-23 01:34:27 +0800

    OOP-HW4

--- src/fcu/iecs/oop/tiida/Main.java
+++ src/fcu/iecs/oop/tiida/Main.java
@@ -1,7 +1,20 @@
 package fcu.iecs.oop.tiida;
 
-public class Main {
+import java.util.Scanner;
 
-	 
-	
+public class Main {
+	public static void main(String[] args) {
+
+		NissanTiida nissantiida = new NissanTiida();
+
+		Scanner scanner = new Scanner(System.in);
+
+		int i,num = 0;
+		System.out.printf("Please enter a number:");
+		num = scanner.nextInt();
+        for(i=0;i<num;i++)
+        {
+        	nissantiida.tiida();
+        }
+	}
 }
--- src/fcu/iecs/oop/tiida/NissanTiida.java
+++ src/fcu/iecs/oop/tiida/NissanTiida.java
@@ -1,5 +1,18 @@
 package fcu.iecs.oop.tiida;
 
 public class NissanTiida {
+	static int count = 1;
+
+	public void tiida() {
+		int i, j;
+
+		for (i = 0; i < count; i++) {
+			for (j = 0; j < count; j++) {
+				System.out.printf("*");
+			}
+			System.out.printf("\n");
+		}
+		count++;
+	}
 
 }
